271784be673bead066ff0f0df1bdfeb96ef1c8b4
james <jsthompson@umail.ucsb.edu>
2015-03-11 22:27:07 -0700

    :space_invaders: added HME.sql

--- src/HealthInformationSystem.java
+++ src/HealthInformationSystem.java
@@ -2,30 +2,34 @@
 public class HealthInformationSystem {
 
     public static void main(String[] args) {
-        String hme_vals[] = new String[]{"pat2", "prov", "patrole", "give", "fam", "suf", "gen", "birth", "xml"};
-        PatientEntity pat = new PatientEntity(hme_vals);
-        pat.run();
+//        String hme_vals[] = new String[]{"pat2", "prov", "patrole", "give", "fam", "suf", "gen", "birth", "xml"};
+//        PatientEntity pat = new PatientEntity(hme_vals);
+//        pat.run();
+//
+//        GuardianEntity grd = new GuardianEntity();
+//        grd.run();
+//
+//        AuthorEntity aut = new AuthorEntity();
+//        aut.run();
+//
+//        InsuranceCompanyEntity icom = new InsuranceCompanyEntity();
+//        icom.run();
+//
+//        FamilyHistoryEntity fhist = new FamilyHistoryEntity();
+//        fhist.run();
+//
+//        LabTestReportEntity ltr = new LabTestReportEntity();
+//        ltr.run();
+//
+//        AllergyEntity alrg = new AllergyEntity();
+//        alrg.run();
+//
+//        PlanEntity pln = new PlanEntity();
+//        pln.run();
 
-        GuardianEntity grd = new GuardianEntity();
-        grd.run();
 
-        AuthorEntity aut = new AuthorEntity();
-        aut.run();
-
-        InsuranceCompanyEntity icom = new InsuranceCompanyEntity();
-        icom.run();
-        
-        FamilyHistoryEntity fhist = new FamilyHistoryEntity();
-        fhist.run();
-        
-        LabTestReportEntity ltr = new LabTestReportEntity();
-        ltr.run();
-        
-        AllergyEntity alrg = new AllergyEntity();
-        alrg.run();
-        
-        PlanEntity pln = new PlanEntity();
-        pln.run();
+        MessageExchangeSystem m = new MessageExchangeSystem();
+        m.run();
     }
     
 }
