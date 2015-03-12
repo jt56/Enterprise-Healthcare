d30d5f349ad876edf3096a7b6a308e34bd5e2263
Heneli Kailahi <heneli@kailahi.com>
2015-03-12 14:05:09 -0700

    Runs HealthMessageExchange now

--- src/HealthInformationSystem.java
+++ src/HealthInformationSystem.java
@@ -2,45 +2,45 @@
 public class HealthInformationSystem {
 
     public static void main(String[] args) {
-        RunSQL r = new RunSQL();
-        r.run("healthInformationSystem.sql");
-        
-        String hme_vals[] = new String[]{"pat2", "prov", "guard", "give", null, "suf", "gen", "birth", "last", "xml"};
-        PatientEntity pat = new PatientEntity(hme_vals);
-        pat.run();
-
-        GuardianEntity grd = new GuardianEntity();
-        grd.run();
-
-        AuthorEntity aut = new AuthorEntity();
-        aut.run();
-
-        InsuranceCompanyEntity icom = new InsuranceCompanyEntity();
-        icom.run();
-
-        FamilyHistoryEntity fhist = new FamilyHistoryEntity();
-        fhist.run();
-
-        LabTestReportEntity ltr = new LabTestReportEntity();
-        ltr.run();
-
-        AllergyEntity alrg = new AllergyEntity();
-        alrg.run();
-
-        PlanEntity pln = new PlanEntity();
-        pln.run();
-
-        VisitsLabRelation vlr = new VisitsLabRelation();
-        vlr.run();
-
-        HasInsuranceCompanyRelation hicr = new HasInsuranceCompanyRelation();
-        hicr.run();
-
-        HasAuthorRelation har = new HasAuthorRelation();
-        har.run();
-
-        HasPlanRelation hpr = new HasPlanRelation();
-        hpr.run();
+//        RunSQL r = new RunSQL();
+//        r.run("healthInformationSystem.sql");
+//        
+//        String hme_vals[] = new String[]{"pat2", "prov", "guard", "give", null, "suf", "gen", "birth", "last", "xml"};
+//        PatientEntity pat = new PatientEntity(hme_vals);
+//        pat.run();
+//
+//        GuardianEntity grd = new GuardianEntity();
+//        grd.run();
+//
+//        AuthorEntity aut = new AuthorEntity();
+//        aut.run();
+//
+//        InsuranceCompanyEntity icom = new InsuranceCompanyEntity();
+//        icom.run();
+//
+//        FamilyHistoryEntity fhist = new FamilyHistoryEntity();
+//        fhist.run();
+//
+//        LabTestReportEntity ltr = new LabTestReportEntity();
+//        ltr.run();
+//
+//        AllergyEntity alrg = new AllergyEntity();
+//        alrg.run();
+//
+//        PlanEntity pln = new PlanEntity();
+//        pln.run();
+//
+//        VisitsLabRelation vlr = new VisitsLabRelation();
+//        vlr.run();
+//
+//        HasInsuranceCompanyRelation hicr = new HasInsuranceCompanyRelation();
+//        hicr.run();
+//
+//        HasAuthorRelation har = new HasAuthorRelation();
+//        har.run();
+//
+//        HasPlanRelation hpr = new HasPlanRelation();
+//        hpr.run();
 
 
 //        pat.retrievePatient( "pat2" );
@@ -53,9 +53,12 @@ public class HealthInformationSystem {
 //        System.out.println(grd.getPatientid());
 //        System.out.println(grd.getGuardianno());
 
-        PatientView pV = new PatientView("pat2asdfasfg");
+        MessageExchangeSystem mes = new MessageExchangeSystem();
+        mes.run();
+        
+        PatientView pV = new PatientView("0");
 
-        PatientView pV2 = new PatientView("pat2");
+        PatientView pV2 = new PatientView("12444");
 
     }
     
